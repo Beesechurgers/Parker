905231a2cc358bbb2204ab859dae176f479d7c13
Shashank Verma <shashank.verma2002@gmail.com>
2021-05-11 10:59:55 +0800

    LoginActivity: Validate inputs
    Signed-off-by: Shashank Verma <shashank.verma2002@gmail.com>

--- app/src/main/java/com/beesechurgers/parker/LoginActivity.java
+++ app/src/main/java/com/beesechurgers/parker/LoginActivity.java
@@ -1,26 +1,60 @@
 package com.beesechurgers.parker;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.View;
-import android.widget.Button;
+import android.util.Patterns;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatEditText;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private static final String TAG = "LoginActivity";
+
+    AppCompatEditText emailInput, passwordInput;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-        Button buttonOne = findViewById(R.id.sign_up);
-        buttonOne.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                Log.d("LoginActivity", "button was clicked");
-                Intent activity2Intent = new Intent(getApplicationContext(), SignUp.class);
-                startActivity(activity2Intent);
+
+        emailInput = findViewById(R.id.login_email);
+        passwordInput = findViewById(R.id.login_password);
+
+        findViewById(R.id.login_btn).setOnClickListener(v -> {
+            String email = validateEmail(), password = validatePassword();
+            if (!email.equals("None") && !password.equals("None")) {
+                Log.d(TAG, "Login: onClick: Continue Login");
             }
         });
+
+        findViewById(R.id.sign_up_btn).setOnClickListener(v -> startActivity(new Intent(this, SignUp.class)));
+    }
+
+    private String validateEmail() {
+        String email = emailInput.getText().toString().trim();
+        if (email.isEmpty()) {
+            emailInput.setError("Empty Email");
+            return "None";
+        } else if (Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+            return email;
+        } else {
+            emailInput.setError("Invalid Email");
+            return "None";
+        }
+    }
+
+    private String validatePassword() {
+        String password = passwordInput.getText().toString().trim();
+        if (password.isEmpty()) {
+            emailInput.setError("Empty Password");
+            return "None";
+        } else if (password.length() < 6) {
+            emailInput.setError("Password length < 6");
+            return "None";
+        } else {
+            return password;
+        }
     }
 }
