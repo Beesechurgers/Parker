949de7e7b2422e4e0a0511cf050c1078147cd751
Shashank Verma <shashank.verma2002@gmail.com>
2021-05-11 11:03:22 +0800

    App: Rename MainActivity -> SplashActivity
    Signed-off-by: Shashank Verma <shashank.verma2002@gmail.com>

--- app/src/main/java/com/beesechurgers/parker/SplashActivity.java
+++ app/src/main/java/com/beesechurgers/parker/SplashActivity.java
@@ -5,12 +5,12 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class SplashActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_splash);
         Intent activity2Intent = new Intent(getApplicationContext(), LoginActivity.class);
         startActivity(activity2Intent);
     }
